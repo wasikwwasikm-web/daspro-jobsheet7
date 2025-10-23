b1494fc3b6ec5c7f49060ecbf74660ea8b177c4e
wasik <wasikwwasikm@gmail.com>
2025-10-23 12:56:23 +0700

    percob1

--- jobseed7/SiakadForNoAbsen.java
+++ jobseed7/SiakadForNoAbsen.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class SiakadForNoAbsen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 100, terendah = 0;
+        int lulus = 0, tidakLulus = 0;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = scanner.nextDouble();
@@ -13,8 +14,15 @@ public class SiakadForNoAbsen {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus ++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
     }
 }
